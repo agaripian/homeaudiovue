650fe7b3beeecd13e6513006ca87cfefd8e40d88
Alex Garipian <agaripian@gmail.com>
2018-11-23 18:14:28 -0500

    Error and Zone components

---
+++ components/Zone.vue
@@ -0,0 +1,198 @@
+<template>
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex
+      :class="$style.buttonContainer"
+      xs2>
+      <v-btn
+        v-if="!isAllZone"
+        v-model="zone.pr"
+        :class="[isOn ? 'green' : 'grey', 'lighten-2 black--text']"
+        fab
+        red
+        small
+        @click="$emit('zoneEvent', { attr: 'pr', value: isOn ? '00':'01' , zone:zone.zone })">
+        <slot>
+          <font-awesome-icon
+            :class="$style.icon"
+            icon="power-off"
+          />
+        </slot>
+      </v-btn>
+      <v-btn
+        v-if="isAllZone"
+        class="grey black--text lighten-2"
+        fab
+        small
+        @click="$emit('zoneEvent', { attr: 'pr', value: '00' , zone:zone.zone })">
+        <v-icon> power_off </v-icon>
+      </v-btn>
+      <v-btn
+        v-if="isAllZone"
+        class="grey black--text lighten-2"
+        fab
+        small
+        @click="$emit('zoneEvent', { attr: 'pr', value: '01' , zone:zone.zone })">
+        <v-icon> power </v-icon>
+      </v-btn>
+    </v-flex>
+    <v-flex
+      xs6>
+      {{ zone.zone }}
+      <v-slider
+        :disabled="!isOn && !isAllZone || isMuted"
+        v-model="zone.vo"
+        :color="sliderColor"
+        :max="38"
+        :min="0"
+        :thumb-label="true"
+        append-icon="volume_up"
+        class="black-text"
+        prepend-icon="volume_down"
+        thumb-color="white"
+        thumb-size="50"
+        @change="$emit('zoneEvent', { attr: 'vo', value: $event, zone:zone.zone })"
+      />
+    </v-flex>
+    <v-flex
+      v-if="!isAllZone"
+      :class="$style.buttonContainer"
+      xs2>
+      <v-btn
+        :disabled="!isOn"
+        :class="[isExpanded ? 'green' : 'grey  black--text lighten-2']"
+        fab
+        red
+        small
+        @click="isExpanded = !isExpanded">
+        <v-icon> settings </v-icon>
+      </v-btn>
+    </v-flex>
+    <v-flex
+      :class="$style.buttonContainer"
+      xs2>
+      <v-btn
+        v-if="!isAllZone"
+        :disabled="!isOn"
+        :class="[isMuted ? 'error' : 'grey  black--text lighten-2']"
+        fab
+        red
+        small
+        @click="$emit('zoneEvent', { attr: 'mu', value: isMuted ? '00':'01' , zone:zone.zone })">
+        <slot>
+          <font-awesome-icon
+            :class="$style.icon"
+            icon="volume-mute"
+          />
+        </slot>
+      </v-btn>
+      <v-btn
+        v-if="isAllZone"
+        class="grey black--text lighten-2"
+        fab
+        red
+        small
+        @click="$emit('zoneEvent', { attr: 'mu', value: '01' , zone:zone.zone })">
+        <slot>
+          <font-awesome-icon
+            :class="$style.icon"
+            icon="volume-mute"
+          />
+        </slot>
+      </v-btn>
+      <v-btn
+        v-if="isAllZone"
+        class="grey black--text lighten-2"
+        fab
+        red
+        small
+        @click="$emit('zoneEvent', { attr: 'mu', value: '00' , zone:zone.zone })">
+        <slot>
+          <font-awesome-icon
+            :class="$style.icon"
+            icon="volume-up"
+          />
+        </slot>
+      </v-btn>
+    </v-flex>
+    <v-flex
+      v-if="isExpanded"
+      xs12>
+      <Options
+        :zone="zone"
+        @zoneEvent="$emit('zoneEvent', $event)"/>
+    </v-flex>
+    <v-flex
+      xs12>
+      {{ zone }}
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+import Options from '~/components/Options'
+
+export default {
+  components: {
+    Options
+  },
+  props: {
+    zone: {
+      type: Object,
+      required: true
+    },
+    optionsExpanded: {
+      type: Boolean,
+      default: () => false,
+      required: false
+    }
+  },
+  data() {
+    return {
+      isExpanded: this.optionsExpanded
+    }
+  },
+  computed: {
+    isOn() {
+      return this.zone.pr === '01'
+    },
+    isAllZone() {
+      return this.zone.zone === 'all'
+    },
+    isMuted() {
+      return this.zone.mu === '01'
+    },
+    sliderColor() {
+      let val = this.zone.vo || 1
+      //const red = 100 + Number(val) * 5
+      const red = (val / 38) * 1.05
+      console.log('red', red)
+      //return `rgb(${red}, 0, 0)`
+      return `rgb(255, 0, 0, ${red})`
+    }
+  }
+}
+</script>
+
+<style module lang="scss">
+.icon {
+  height: 18px;
+}
+
+.buttonContainer {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+</style>
+
+<style>
+.v-slider__thumb-label.white {
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,75 @@
 <template>
-  <v-container
-    fluid
-    grid-list-lg
-  >
-    <Error
-      v-if = "error"
-      :message = "error"
-      @retry="reload"
-    />
-    <Zone
-      v-for="zone in zones"
-      v-if="zone"
-      :key="zone.zone"
-      :zone="zone"
-      @zoneEvent="zoneCall($event)"
-    />
-  </v-container>
-
+  <div>
+    <v-toolbar
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title"/>
+      <v-btn
+        icon
+        @click.stop="isSettingsDialogOpen = true"
+      >
+        <v-icon>settings</v-icon>
+      </v-btn>
+      <v-btn
+        flat
+        @click.stop="isControlAllDialogOpen = true"
+      >
+        <v-icon left>equalizer</v-icon>
+        Control All
+      </v-btn>
+      <v-btn
+        flat
+        @click.stop="allZoneCall({ attr: 'pr', value: '00' })"
+      >
+        <v-icon left>power_off</v-icon>
+        All Off
+      </v-btn>
+    </v-toolbar>
+    <v-container
+      fluid
+      grid-list-lg
+    >
+      <Error
+        v-if = "error"
+        :message = "error"
+        @retry="reload"
+      />
+      <Zone
+        v-for="zone in zones"
+        v-if="zone"
+        :key="zone.zone"
+        :zone="zone"
+        @zoneEvent="zoneCall($event)"
+      />
+    </v-container>
+    <v-dialog
+      v-model="isSettingsDialogOpen"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition">
+      <Settings
+        :zones="zones"
+        @close="isSettingsDialogOpen=false"/>
+    </v-dialog>
+    <v-dialog
+      v-model="isControlAllDialogOpen"
+      hide-overlay
+      transition="dialog-transition">
+      <ControlAll
+        :zone="masterZone"
+        @zoneEvent="allZoneCall($event)"
+        @close="isControlAllDialogOpen=false"/>
+    </v-dialog>
+  </div>
 </template>
 
 
 <script>
 import Zone from '~/components/Zone'
 import Error from '~/components/Error'
+import Settings from '~/components/Settings'
+import ControlAll from '~/components/ControlAll'
 
 function pad2(val) {
   if (typeof val === 'string') {
@@ -35,14 +82,33 @@ function pad2(val) {
 export default {
   components: {
     Zone,
-    Error
+    Error,
+    Settings,
+    ControlAll
   },
 
   data() {
     return {
       zones: [],
-      error: ''
+      error: '',
+      title: 'HomeAudio',
+      isSettingsDialogOpen: false,
+      isControlAllDialogOpen: false,
+      // eslint-disable-next-line
+      masterZone: { 'zone': 'all', 'pa': '00', 'pr': '00', 'mu': '00', 'dt': '00', 'vo': '15', 'tr': '07', 'bs': '07', 'bl': '10', 'ch': '01', 'ls': '00' }
     }
+  },
+
+  computed: {
+    // masterZone() {
+    //   if (!this.zones.length) {
+    //     return {}
+    //   }
+    //   const masterZone = { ...this.zones[0] }
+    //   masterZone.zone = 'all'
+    //   console.log('masterZone', masterZone)
+    //   return masterZone
+    // }
   },
 
   async asyncData({ app }) {
@@ -59,14 +125,7 @@ export default {
       }
     }
   },
-
   methods: {
-    // async volChange(val, zone) {
-    //   const formattedVal = pad2(val)
-    //   console.log('volChange', formattedVal)
-    //   await this.zoneCall('vo', val, zone)
-    // },
-
     async zoneCall({ attr, value, zone }) {
       try {
         const formattedVal = pad2(value)
@@ -85,6 +144,54 @@ export default {
       } catch (error) {
         showError(`There was an error setting ${attr}`)
       }
+    },
+
+    async allZoneCall({ attr, value }) {
+      try {
+        this.masterZone[attr] = value
+        const formattedVal = pad2(value)
+        const result = await this.$axios.post(
+          `/allzones/${attr}`,
+          formattedVal,
+          {
+            headers: { 'Content-Type': 'text/plain' }
+          }
+        )
+        this.zones = result.data
+      } catch (error) {
+        showError(`There was an error setting allZones ${attr}`)
+      }
+    },
+
+    updateMasterZone(zones) {
+      // let masterValues = { pr: '00', mu: '00', ch: '01' }
+      // let { pr, mu, ch } = zonez[0]
+      // let prMatch = true
+      // let muMatch = true
+      // zones.every(zone => {
+      //   if (zone.pr !== pr) {
+      //     prMatch = false
+      //   }
+      //   if (zone.mu !== mu) {
+      //     prMatch = false
+      //   }
+      // })
+      // newValue.forEach(zone => {
+      //   console.log('zone', zone.zone)
+      //   Object.keys(zone).forEach(key => {
+      //     if (!masterValues[key]) {
+      //       return
+      //     }
+      //     if (!masterPower) {
+      //       masterPower = masterValues['pr']
+      //     } else {
+      //       if ( masterPower !== masterValues['pr'])
+      //     }
+      //     masterValues[key] = zone[key]
+      //   })
+      // })
+      // Object.assign(this.masterZone, masterValues)
+      console.log('masterZone', this.masterZone)
     },
 
     reload() {
